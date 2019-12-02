f01608c717045751c09b2e73e15aee897563643c
Denis Popov <info@de4me.com>
2019-12-02 18:12:34 +0300

    New path_delete_last_component(); Optimization;

--- SourceX/utils.cpp
+++ SourceX/utils.cpp
@@ -1,13 +1,10 @@
 #include "cross_header.h"
 #include "utils.h"
 
-
-size_t path_copy(char* dst, size_t size, const char* src){
+size_t path_add_separator(char* dst, size_t size){
 	if(dst==NULL || size==0) return 0;
-	if(src==NULL) return 0;
-	size_t length = strlen(src);
-	if(length==0 || length>=size) return 0;
-	strncpy(dst, src, size);
+	size_t length = strlen(dst);
+	if(length==0) return 0;
 	switch (dst[length-1]) {
 		case '\\':
 		case '/':
@@ -19,6 +16,35 @@ size_t path_copy(char* dst, size_t size, const char* src){
 #else
 			dst[length++] = '/';
 #endif
+			dst[length] = 0;
+			break;
 	}
 	return length;
+}
+
+size_t path_copy(char* dst, size_t size, const char* src){
+	if(dst==NULL || size==0) return 0;
+	if(src==NULL) return 0;
+	size_t length = strlen(src);
+	if(length==0 || length>=size) return 0;
+	if(dst!=src) strncpy(dst, src, size);
+	return path_add_separator(dst, size);
+}
+
+size_t path_delete_last_component(char* dst, size_t size, const char* src){
+	if(dst==NULL || size==0) return 0;
+	if(src==NULL) return 0;
+	for(size_t i = strlen(src); i>0; i-- ){
+		switch (src[i-1]) {
+			case '\\':
+			case '/':
+				if(i>=size) return 0;
+				if(dst!=src) strncpy(dst, src, i);
+				dst[i] = 0;
+				return i;
+			default:
+				break;
+		}
+	}
+	return 0;
 }
--- SourceX/utils.h
+++ SourceX/utils.h
@@ -1,10 +1,20 @@
 #pragma once
 
 /**
-	Copy path from src to dst. Add last path character to end if not exist.
-	@param dst Destination string
-	@param size The size of the destination string, in characters
-	@param src Source string
-	@return Number bytes copied
+ Copy a path string.
+ The return path will be guaranteed to end with a path separator.
+ @param dst Location of the destination string buffer
+ @param size Size of the destination string buffer in char units
+ @param src Null-terminated source string buffer
+ @return The number of characters copied
 */
 size_t path_copy(char* dst, size_t size, const char* src);
+
+/**
+ Creates a copy of a given path with the last path component deleted.
+ @param dst Location of the destination string buffer
+ @param size Size of the destination string buffer in char units
+ @param src Null-terminated source string buffer
+ @return The number of characters copied
+*/
+size_t path_delete_last_component(char* dst, size_t size, const char* src);
